e4e5100f6e49e34478048e4c3ef630231536c71c
Paul Logston <code@logston.me>
2015-02-05 23:51:07 -0500

    Refactor test module into test package.

---
+++ tests/constants.py
@@ -0,0 +1,5 @@
+import os
+
+BUCKET = os.environ['AWS_S3_BUCKET']
+ACCESS_KEY = os.environ['AWS_S3_ACCESS_KEY']
+SECRET_KEY = os.environ['AWS_S3_SECRET_KEY']
--- tests/test_upload_download.py
+++ tests/test_upload_download.py
@@ -1,13 +1,9 @@
-import os
 import unittest
 from datetime import datetime
 
 from openS3 import OpenS3
 
-
-bucket = os.environ['AWS_S3_BUCKET']
-access_key = os.environ['AWS_S3_ACCESS_KEY']
-secret_key = os.environ['AWS_S3_SECRET_KEY']
+from .constants import BUCKET, ACCESS_KEY, SECRET_KEY
 
 
 class OpenS3TestCase(unittest.TestCase):
@@ -24,34 +20,34 @@ class OpenS3TestCase(unittest.TestCase):
         pass
 
     def test_content_type(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             self.assertEqual(fd.content_type, 'text/plain')
 
     def test__101_head_file_does_not_exist(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             self.assertFalse(fd.exists())
 
     def test__102_put_object_in_bucket(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key, mode='wb') as fd:
             fd.write(self.content)
 
     def test__301_head_file_does_exist(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             self.assertTrue(fd.exists())
 
     def test__501_get_object_from_bucket(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             content = fd.read().decode()
             self.assertEqual(content, self.content)
             self.assertEqual(fd.headers['Content-Type'], 'text/plain')
 
     def test__701_delete_object_from_bucket(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             fd.delete()
             self.assertFalse(fd.exists())
@@ -70,34 +66,34 @@ class OpenS3LargeFileTestCase(unittest.TestCase):
         self.object_key = '/testdir/large_test.txt'
 
     def test_content_type(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             self.assertEqual(fd.content_type, 'text/plain')
 
     def test__101_head_file_does_not_exist(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             self.assertFalse(fd.exists())
 
     def test__102_put_object_in_bucket(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key, mode='wb') as fd:
             fd.write(self.content)
 
     def test__301_head_file_does_exist(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             self.assertTrue(fd.exists())
 
     def test__501_get_object_from_bucket(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             content = fd.read().decode()
             self.assertEqual(content, self.content)
             self.assertEqual(fd.headers['Content-Type'], 'text/plain')
 
     def test__701_delete_object_from_bucket(self):
-        openS3 = OpenS3(bucket, access_key, secret_key)
+        openS3 = OpenS3(BUCKET, ACCESS_KEY, SECRET_KEY)
         with openS3(self.object_key) as fd:
             fd.delete()
             self.assertFalse(fd.exists())
